ef48883995c5d1610819dd834d15e41daee044a2
mario malak <93185168+mariomalak1@users.noreply.github.com>
2024-01-28 12:30:44 +0200

    edit configs

--- Market/__init__.py
+++ Market/__init__.py
@@ -5,6 +5,10 @@ from flask_bcrypt import Bcrypt
 
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = "1e6fcc6160a5d633efd78b334c5416gd"
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///MarketApp.db"
+
 db = SQLAlchemy(app)
 by = Bcrypt(app)
 login_manger = LoginManager(app)
@@ -13,8 +17,6 @@ login_manger.login_view = 'user.login'
 
 login_manger.login_message_category = 'warning'
 login_manger.login_message = "سجل الدخول اولا"
-app.config['SECRET_KEY'] = "1e6fcc6160a5d633efd78b334c5416gd"
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///MarketApp.db"
 
 from Market.User.routes import user
 from Market.Buyer.routes import buyer
